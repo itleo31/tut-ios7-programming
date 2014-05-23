c740acde3ef6d432a8542aa03495d38b7dbc7de7
Khanh Pham <khanhpham@Khanhs-Mac.local>
2014-05-24 00:08:51 +0700

    Finish DynamicPlayout \n Add SandSwitch project

--- source-code/DynamicsPlayGround/DynamicsPlayGround/ViewController.m
+++ source-code/DynamicsPlayGround/DynamicsPlayGround/ViewController.m
@@ -16,6 +16,7 @@
     UIDynamicAnimator *_animator;
     UIGravityBehavior *_gravity;
     UICollisionBehavior *_collision;
+    BOOL _firstContact;
 }
 
 - (void)viewDidLoad
@@ -37,11 +38,38 @@
     
     _collision = [[UICollisionBehavior alloc] initWithItems:@[square]];
     _collision.translatesReferenceBoundsIntoBoundary = YES;
+    _collision.collisionDelegate = self;
     [_animator addBehavior:_collision];
+    
+    UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[square]];
+    itemBehavior.elasticity = 0.6;
+    [_animator addBehavior:itemBehavior];
     
     // add a boundary
     CGPoint rightEdge = CGPointMake(barrier.frame.origin.x + barrier.frame.size.width, barrier.frame.origin.y);
     [_collision addBoundaryWithIdentifier:@"barrier" fromPoint:barrier.frame.origin toPoint:rightEdge];
+}
+
+- (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    UIView* view = (UIView*)item;
+    view.backgroundColor = [UIColor yellowColor];
+    [UIView animateWithDuration:0.3 animations:^{
+        view.backgroundColor = [UIColor grayColor];
+    }];
+    
+    if (!_firstContact) {
+        _firstContact = YES;
+        UIView *square = [[UIView alloc] initWithFrame:CGRectMake(30, 0, 100, 100)];
+        square.backgroundColor = [UIColor grayColor];
+        [self.view addSubview:square];
+        
+        [_collision addItem:square];
+        [_gravity addItem:square];
+        
+        UIAttachmentBehavior *attach = [[UIAttachmentBehavior alloc] initWithItem:view attachedToItem:square];
+        [_animator addBehavior:attach];
+    }
 }
 
 - (void)didReceiveMemoryWarning
